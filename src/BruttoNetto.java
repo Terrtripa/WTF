52dd0112919cace2516bfced2724946d4f4a6d18
Terrtripa <monicaburila@gmail.com>
2021-03-04 00:28:33 +0100

    Commit

---
+++ src/BruttoNetto.java
@@ -0,0 +1,22 @@
+public class BruttoNetto {
+
+    public static void main() {
+            int grade = 4;
+            if (grade == 1) {
+                System.out.println("Note : Sehr gut");
+            } else if (grade == 2) {
+                System.out.println("Note : Gut");
+            } else if (grade == 3) {
+                System.out.println("Note : Befriedigend");
+            } else if (grade == 4) {
+
+            } else {
+                System.out.println("Note : Schlecht");
+            }
+    }
+    public static void printNameToGrade (int Grade) {
+        // Aufgabe => Namen für Note}
+
+        }
+        printNameToGrade(1);
+    }
