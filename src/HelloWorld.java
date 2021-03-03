d30a0317864f574a81ba006d7c0d04f9675e4e28
Nowa <nowa@zam-nowa.local>
2021-03-03 19:43:42 +0100

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,24 @@
+
+//Komentar
+/*
+   1. Klassename muss Dateiname entsprechen
+   2. Ändern => Refactor (Shift + F6)
+ */
+
+public class HelloWorld {
+
+    // 3. psvm+Enter generiert uns die main-Methode
+    public static void main(String[] args) {
+        // Startpunkt für unsere Programme
+
+        System.out.println("Hello World on day 4321");
+        // Liebes "System"
+        // wir wollen etwas "out" ausgeben
+        // print
+        // ln (neue zeile)
+        // übergen mit "..." einen text
+        // Abkürzung "sout"+Enter = System.out.println();
+
+
+    }
+}
