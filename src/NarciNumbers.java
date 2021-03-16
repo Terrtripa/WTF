9c0eaadb9d2fb9dd90a66c8eec86a6dbeb1a0909
Terrtripa <monicaburila@gmail.com>
2021-03-16 11:43:40 +0100

    Bla

--- src/NarciNumbers.java
+++ src/NarciNumbers.java
@@ -2,6 +2,20 @@ public class NarciNumbers {
     public static void main(String[] args) {
 
     }
+   public static boolean calcNarci(int number){
+       int nrDigits = 0;
+
+        int sum = 0;
+       for (int i = 1; i <= 1000 ; i++) {
+           if ( ){
+               sum = number;
+               return true;
+           }return false;
+
+       }
+
+       }
+   }
 
 
 }
